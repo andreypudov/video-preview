81223edf5333c4e71c55f2d600479e47862601a7
Andrey Pudov <mail@andreypudov.com>
2023-10-06 15:53:46 +0200

    Hide controls, use loop

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const addVideo = async () => {
   const video = document.createElement('video');
 
   video.src = 'videos/2.mp4';
-  video.controls = true;
+  video.controls = false;
   video.autoplay = true;
+  video.loop = true;
 
   document.body.appendChild(video);
 }
