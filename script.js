6b0deeba7f00fe4243a28fb4b286bdd3b0893df7
Andrey Pudov <mail@andreypudov.com>
2023-10-10 14:20:28 +0200

    ~ added paged structure

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const addVideo = () => {
   const button = document.querySelector('button');
   const video = document.createElement('video');
 
-  video.src = 'videos/2.mp4';
+  video.src = '../videos/2.mp4';
   video.controls = false;
   video.autoplay = true;
   video.loop = true;
